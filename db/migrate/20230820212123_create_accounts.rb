985c4c25de70ad5c789bae3bf64371ffdff2ed57
raonircl <raoni.afi@gmail.com>
2023-08-20 18:23:20 -0300

    feat: add models supplier, account, account_history and db:migrate

---
+++ db/migrate/20230820212123_create_accounts.rb
@@ -0,0 +1,10 @@
+class CreateAccounts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :accounts do |t|
+      t.integer :number
+      t.references :supplier, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
